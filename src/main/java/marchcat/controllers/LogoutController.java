49c8820ca7b35f35b4f364cdb3ec9cd3af42f6c2
Toyotadriver <3yazik@gmail.com>
2025-09-23 00:28:41 +0300

    Logout

---
+++ src/main/java/marchcat/controllers/LogoutController.java
@@ -0,0 +1,26 @@
+package marchcat.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import marchcat.users.Logged;
+
+@Controller
+public class LogoutController {
+
+	private Logged logged;
+	
+	public LogoutController(Logged logged) {
+		this.logged = logged;
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		if(logged.getUsername() != null) {
+			logged.setUsername(null);
+			logged.setId(-1);
+		}
+		return "redirect:/main";
+	}
+	
+}
--- src/main/java/marchcat/controllers/MainController.java
+++ src/main/java/marchcat/controllers/MainController.java
@@ -1,13 +1,25 @@
 package marchcat.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import marchcat.users.Logged;
 
 @Controller
 public class MainController {
+	
+	Logged logged;
+	
+	public MainController(Logged logged) {
+		this.logged = logged;
+	}
 
 	@GetMapping("/main")
-	public String main() {
+	public String main(Model model) {
+		String l = logged.getUsername();
+		boolean loggedIn = (l != null);
+		model.addAttribute("loggedIn", loggedIn);
 		return "main.html";
 	}
 }
